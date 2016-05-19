e3056f8b9681064ad490c10f15bbf475532b2d87
Felix Garrido <felix.garrido@gmail.com>
2016-05-19 13:15:14 +0200

    Prueba de modificación de archivo con git-plus de Atom

--- gulpfile.js
+++ gulpfile.js
@@ -5,21 +5,20 @@ var gulp       = require('gulp'),
     $build     = 'build',
     $port      = 3008,
     $address   = ip.address();
-    
-gulp.task('server', function() {
-	var server = gls.static($build, $port);
-	server.start();
-    
-    var options = {
-        uri: 'http://'+ $address +':' + $port,
-        app: 'google chrome'
-    };
-    gulp.src(__filename)
-        .pipe(open(options));
 
-	gulp.watch([$build + '/*.html',$build + '/css/*.css',$build + '/js/*.js'], function (file) {
-		server.notify.apply(server, [file]);
-	});
+gulp.task('server', function() {
+  var server = gls.static($build, $port);
+  server.start();
+  var options = {
+      uri: 'http://'+ $address +':' + $port,
+      app: 'google chrome'
+  };
+  gulp.src(__filename)
+      .pipe(open(options));
+
+  gulp.watch([$build + '/*.html',$build + '/css/*.css',$build + '/js/*.js'], function (file) {
+  	server.notify.apply(server, [file]);
+  });
 });
 
 gulp.task('default', ['server']);
